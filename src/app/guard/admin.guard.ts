e537c3d9521cba31da4e5ddc2c4e45a00ff0eaa0
Victor <victorsmirnov@me.com>
2022-05-24 22:53:54 +0300

    1111

--- src/app/guard/admin.guard.ts
+++ src/app/guard/admin.guard.ts
@@ -11,7 +11,10 @@ export class AdminGuard implements CanActivateChild {
   isAdmin: Observable<boolean> | undefined;
 
   constructor(private auth: AuthService, private router: Router) {}
-  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+  
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot, 
+    state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     this.auth.onAdmin().subscribe(res => this.isAdmin = res);
     return this.isAdmin ? true : false;
   }
